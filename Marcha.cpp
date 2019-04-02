e1ce80cdd858961206706d66a9795c2be268e78d
lcesquivel10 <48950395+lcesquivel10@users.noreply.github.com>
2019-04-02 07:53:31 -0500

    Update Marcha.cpp

--- Marcha.cpp
+++ Marcha.cpp
@@ -4,10 +4,10 @@
 int main(){
     int i;
     int n; n= 1000;
-    int x;x=0; int y ; y=0; float pi; float pi =3.14159;
+    int x;x=0; int y ; y=0; float p; float p =3.14159;
   for (i=0;i<n;i++){
      float r;
-    r= srand()2*pi;x+=sin(r);y+= sin (r) 
+    r= srand()2*p;x+=sin(r);y+= sin (r) 
     std::cout << i << " " <<exp(i) << std::endl;
   } 
   return 0;
