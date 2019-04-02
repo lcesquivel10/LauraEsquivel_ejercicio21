7d0e8b7c978b625e5c66dfa721264b3a98869ca6
lcesquivel10 <48950395+lcesquivel10@users.noreply.github.com>
2019-04-02 07:46:13 -0500

    Update and rename hola.cpp to Marcha.cpp

---
+++ Marcha.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    int i;
+    int n; n= 1000;
+    int x;x=0; int y ; y=0; float pi; float pi =3.14159;
+  for (i=0;i<n;i++){
+     float r;
+    r= srand()2*pi;x+=sin(r);y+= sin (r) 
+    std::cout << i << " " <<exp(i) << std::endl;
+  } 
+  return 0;
+}
